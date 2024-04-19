6981414bf024e95b046467602c35d042dd9ac5cd
Tiago Tavares <tiagoft@gmail.com>
2024-04-19 20:57:58 +0000

    Refactoring goes on...

--- package/src/graph_structure/graph_structure.py
+++ package/src/graph_structure/graph_structure.py
@@ -16,7 +16,7 @@ def _compute_jaccard_similarity(sx, sy):
     return len(sx.intersection(sy)) / len(sx.union(sy))
 
 
-def _mean_neighborhood_similarity(nx, ny, k):
+def mean_neighborhood_similarity_from_neighborhood(nx, ny, k):
     num_points = nx.shape[0]
     inter = 0
     for i in range(num_points):
@@ -27,40 +27,10 @@ def _mean_neighborhood_similarity(nx, ny, k):
     return inter
 
 
-def _mean_neighborhood_distance(nx, ny, k):
-    return 1 - _mean_neighborhood_similarity(nx, ny, k)
-
-
-def mean_neighborhood_similarity(A, B, k):
+def mean_neighborhood_similarity_from_points(A, B, k):
     """
     This is $D_g(A, B, k)$
     """
     nx = nearest_neighbors(A)
     ny = nearest_neighbors(B)
-    return _mean_neighborhood_similarity(nx, ny, k)
-
-
-def mean_neighborhood_distance(A, B, k):
-    """
-    This is $D_g(A, B, k)$
-    """
-    nx = nearest_neighbors(A)
-    ny = nearest_neighbors(B)
-    return _mean_neighborhood_distance(nx, ny, k)
-
-
-def _mean_structural_distance(nx, ny):
-    num_points = nx.shape[0]
-    k_vals = list(range(1, num_points - 1))
-    ns = np.array([_mean_neighborhood_distance(nx, ny, k) for k in k_vals])
-    msd = np.max(1 - ns)
-    return msd
-
-
-def mean_structural_distance(A, B):
-    """
-    This is $S_g(A, B)$
-    """
-    nx = nearest_neighbors(A)
-    ny = nearest_neighbors(B)
-    return _mean_structural_distance(nx, ny)
+    return mean_neighborhood_similarity_from_neighborhood(nx, ny, k)
